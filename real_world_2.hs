17a7cfd976c950254e036b7ba7cab3078e8b815e
ppae <andeppsc@gmail.com>
2019-04-09 12:14:54 +0300

    -

--- real_world_2.hs
+++ real_world_2.hs
@@ -1,6 +1,9 @@
 import Data.Array
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Writer
+import Control.Monad.Reader
+import Control.Monad.State
 import Data.Char
 import Data.List
 import qualified Data.Map as Map
@@ -30,6 +33,34 @@ foldA f s a = go s (indices a)
 
 foldA1 :: Ix k => (a -> a -> a) -> Array k a -> a
 foldA1 f a = foldA f (a ! fst (bounds a)) a
+
+
+
+-- data Writer w a = Writer { runWriter :: (a, w)  }
+-- Reader r a = Reader {  runReader :: r -> a  }
+-- State s a = State { runState :: s -> (a, s)  }
+
+
+half :: Int -> Writer String Int
+half x = do
+            tell ("I just halved " ++ (show x) ++ "!")
+            return (x `div` 2)
+
+hello :: Reader String String
+hello = do
+        name <- ask
+        return ("hello, " ++ name ++ "!\n")
+
+bye :: Reader String String
+bye = do
+        name <- ask
+        return ("bye, " ++ name ++ "!\n")
+
+convo :: Reader String String
+convo = do
+        c1 <- hello
+        c2 <- bye
+        return $ c1 ++ c2
 
 
 main = do
@@ -65,4 +96,11 @@ main = do
             r = Seq.fromList [5,6,8]
         print $ l >< r
         print $ toList (Seq.fromList [1..5])
+
+
+        print $ half 8 >>= half >>= half
+        print $ half <=< half <=< half $ 8 --- monad composition
+        print $ runWriter $ half 8
+        putStrLn $ runReader convo $ "Hasky"
+
 
